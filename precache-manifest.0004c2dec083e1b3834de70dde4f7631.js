083e49b8d4271c1fbb421aa59dc9f644fe3f9f35
Mihail Bondarenco <441044@student.fontys.nl>
2020-11-24 13:46:25 +0100

    Updates

---
+++ precache-manifest.0004c2dec083e1b3834de70dde4f7631.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3afd11ebfd1ff2bedc1c49a9752af0a",
+    "url": "/kozi-frontend/index.html"
+  },
+  {
+    "revision": "c4f7b8102525fc7dc772",
+    "url": "/kozi-frontend/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7d0f8bb09571c09db0d9",
+    "url": "/kozi-frontend/static/css/main.85440ab5.chunk.css"
+  },
+  {
+    "revision": "c4f7b8102525fc7dc772",
+    "url": "/kozi-frontend/static/js/2.a5bfc4a1.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/kozi-frontend/static/js/2.a5bfc4a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0f8bb09571c09db0d9",
+    "url": "/kozi-frontend/static/js/main.327f4909.chunk.js"
+  },
+  {
+    "revision": "c3037a1452abbaa3968e",
+    "url": "/kozi-frontend/static/js/runtime-main.25700bee.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/kozi-frontend/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "47d1f062ccc8aa896d56abb8fcd2b01e",
+    "url": "/kozi-frontend/static/media/map-floor-3.47d1f062.png"
+  }
+]);
